c69da1d9e65ade60a289ac719234c0ea33dbcfdf
Thierry Falvo <thierry.falvo@gmail.com>
2013-04-24 09:53:15 +0200

    Initial import

---
+++ src/GitSample.java
@@ -0,0 +1,13 @@
+
+public class GitSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Démarrage de l'application
+		System.out.println("Mon application Git.");
+	}
+
+}
