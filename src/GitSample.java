dc1b8b0259ac27eebac40fb203e6b96f16650c6c
Thierry Falvo <thierry.falvo@gmail.com>
2013-04-24 09:55:57 +0200

    Nouvelle fonctionnalité expé.

--- src/GitSample.java
+++ src/GitSample.java
@@ -9,6 +9,9 @@ public class GitSample {
 		// Démarrage de l'application
 		System.out.println("Mon application Git.");
 		System.out.println("Démarrage en cours...");
+		
+		// Une nouvelle fonction expé
+		System.out.println("Nouvelle fonctionnalité.");
 	}
 
 }
